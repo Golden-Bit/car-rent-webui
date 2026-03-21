039c90c79ec2989233270b91b5d89acbb47f6f4e
GOLD SOLAR s.r.l <info@goldsolarweb.com>
2026-03-21 12:56:49 +0100

    add booking confirm and management pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,14 +29,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "2d1fce0121bb767b98e4e8b18a34ca54
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "8ebc61e6794f4e45e8fc6829c2950851",
+"flutter_bootstrap.js": "e50b92bd028f0e91d50d82392b119698",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "73be6899f77766217ea0bd2a2c2cd9fb",
 "/": "73be6899f77766217ea0bd2a2c2cd9fb",
-"main.dart.js": "08419b7630e695536bdd91919ffcb4a9",
+"main.dart.js": "9c2778d5f72ede4906e3fc62b7571ed8",
 "manifest.json": "1f06415c39d0263f174797bd989b18b8",
 "version.json": "75d738de6becab89af46a4c6c75b0093"};
 // The application shell files that are downloaded before a service worker can
